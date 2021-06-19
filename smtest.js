af7dc2e6a3d622da1acb98541fc7dca8bb47c1bf
Samuel de Jesús <76408737+Samu330@users.noreply.github.com>
2021-06-19 18:32:29 -0500

    Update smtest.js

--- smtest.js
+++ smtest.js
@@ -1,54 +1,27 @@
 const { WAConnection } = requiere('@adiwajshing/baileys');
 
-async function connectToWhatsApp () {
-
-    const conn = new WAConnection() 
-
-    // called when WA sends chats
-
-    // this can take up to a few minutes if you have thousands of chats!
-    conn.on('chats-received', async ({ hasNewChats }) => {
-
-        console.log(`you have ${conn.chats.length} chats, new chats available: ${hasNewChats}`)
-
-        const unread = await conn.loadAllUnreadMessages ()
-
-        console.log ("you have " + unread.length + " unread messages")
-
+async function iniciar () { 
+    const client = new WAConnection() 
+    client.logger.level = 'warn'
+    console.log(banner.string)
+//llamar al código QR
+    client.on('qr', () => {
     })
+//crear un archivo Json para guardar información: ID del cliente, Token y Keys del cliente y del SERVER.
+	fs.existsSync('./Samu330.json') && client.loadAuthInfo('./Samu330.json')
+//Conectando o reconectando
+	client.on('connecting', () => {
+        console.log('Conectando')
+	})
+//La conexión fue en éxito👌🏻
+	client.on('open', () => {
+	console.log('Conectado exitosamente :D')
+	})
+	await client.connect({timeoutMs: 30*1000})
+        fs.writeFileSync('./Samu330.json', JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
 
-    // called when WA sends chats
+   } 
 
-    // this can take up to a few minutes if you have thousands of contacts!
-
-    conn.on('contacts-received', () => {
-
-        console.log('you have ' + Object.keys(conn.contacts).length + ' contacts')
-
-    })
-
-    await conn.connect ()
-
-    conn.on('chat-update', chatUpdate => {
-
-        // `chatUpdate` is a partial object, containing the updated properties of the chat
-
-        // received a new message
-
-        if (chatUpdate.messages && chatUpdate.count) {
-
-            const message = chatUpdate.messages.all()[0]
-
-            console.log (message)
-
-        } else console.log (chatUpdate) // see updates (can be archived, pinned etc.)
-
-    })
-
-}
-
-// run in main file
-
-connectToWhatsApp ()
+iniciar ()
 
 .catch (err => console.log("unexpected error: " + err)
