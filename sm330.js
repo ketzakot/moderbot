b426a0919d63b9d2bc9adef7374c26e0a343c269
Samuel de Jesús <76408737+Samu330@users.noreply.github.com>
2021-08-23 15:23:14 -0500

    Update sm330.js

--- sm330.js
+++ sm330.js
@@ -79,7 +79,12 @@ const pushname = sam.key.fromMe ? client.user.name : conts.notify || conts.vname
 switch (command) {
 
 case 'bot':
-client.sendMessage(from, 'Hola,felicidades, has logrado enviar un mensaje mediante un servidor externo😚', text, {quoted : sam})
+client.sendMessage(from, 'Hola, felicidades, has logrado enviar un mensaje mediante un servidor externo😚', text, {quoted : sam})
+break
+                
+case 'foto':
+const imagen = fs.readFileSync('./media/logo.jpg')                
+client.sendMessage(from, imagen, MessageType.imagen)
 break
                 
 }
