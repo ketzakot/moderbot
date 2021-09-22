aa5de13c080b92c2f0e6df2bfbe8e417f9dd2071
Samuel de Jesús <76408737+Samu330@users.noreply.github.com>
2021-09-22 11:23:13 -0500

    Update sm330.js

--- sm330.js
+++ sm330.js
@@ -133,6 +133,39 @@ case 'botones':
 boton(from, 'Hola', `${pushname}`, [{buttonId: 'b1', buttonText: {displayText: 'Click Aqui'}, type: 1}])           
 break
                 
+case 'lista':
+let lista = client.prepareMessageFromContent(from,{
+"listMessage": {
+"title": `${pushname}`,
+"description": `Este es un Mensaje de lista`,
+"buttonText": "Click Aqui",
+"listType": "SINGLE_SELECT",
+"sections": [
+{ "title": `Seccion 1`,
+"rows": [
+{
+"title": 'Lista 1',
+"description": 'Desc 1',
+"rowId": `row1`
+}
+]
+},
+{
+"title": `Seccion 2`,
+"rows": [
+{
+"title": 'Lista 2',
+"description": 'Desc 2',
+"rowId": `row 2`
+}
+]    
+}
+]
+}
+}, {quoted: sam})
+client.relayWAMessage(lista)          
+break
+                
 }
 
 } catch (e) {
